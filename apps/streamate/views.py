fd86c076063c9ca2f75a53f9c83abb169904f9ba
gathermate <castwhendamagedealt@gmail.com>
2019-03-21 14:28:42 +0900

    change download file names

--- apps/streamate/views.py
+++ apps/streamate/views.py
@@ -32,20 +32,28 @@ def index():
     return render_template('player.html',
                            streamer=streamer)
 
-@streamate.route('/<string:streamer>/resource')
+@streamate.route('/<path:streamer>/resource')
 def resource(streamer):
     query = MultiDict(request.args.iteritems(multi=True))
     return _order(streamer, 'resource', query)
 
-@streamate.route('/<string:streamer>', strict_slashes=False)
+@streamate.route('/<path:streamer>', strict_slashes=False)
 @auth.requires_auth
 def streamer(streamer):
     return streamer
     return render_template('player.html',
                            streamer=streamer)
 
+@streamate.route('/<path:streamer>/<string:cid>')
+@auth.requires_auth
+def streamer_channel_streaming(streamer, cid):
+    qIndex = int(request.args.get('q', -1))
+    gen = _order(streamer, 'streaming', None)
+    response = Response(stream_with_context(gen(cid, qIndex)), mimetype='video/MP2T')
+    response.headers['Content-Disposition'] = 'attachment; filename={}-{}.ts'.format(streamer, cid)
+    return response
 
-@streamate.route('/<string:streamer>/channels')
+@streamate.route('/<path:streamer>/channels')
 @auth.requires_auth
 def streamer_channels(streamer):
     query = MultiDict(request.args.iteritems(multi=True))
@@ -56,40 +64,31 @@ def streamer_channels(streamer):
             yield info + '\n'
     return Response(stream_with_context(gen()), mimetype='text/plain')
 
-@streamate.route('/<string:streamer>/m3u')
+@streamate.route('/<path:streamer>/<string:filename>.m3u')
 @auth.requires_auth
-def streamer_m3u(streamer):
+def streamer_m3u(streamer, filename):
     query = MultiDict(request.args.iteritems(multi=True))
     m3u_gen = _order(streamer, 'm3u', query)
     response = Response(stream_with_context(m3u_gen), mimetype='application/x-mpegURL')
-    response.headers['Content-Disposition'] = 'attachment; filename=gathermate-{}.m3u'.format(streamer)
+    response.headers['Content-Disposition'] = 'attachment; filename={}.m3u'.format(filename)
     return response
 
-@streamate.route('/m3u')
+@streamate.route('/<string:filename>.m3u')
 @auth.requires_auth
-def m3u():
+def m3u(filename):
     query = MultiDict(request.args.iteritems(multi=True))
     m3u_gen = app.managers[name].order_all_m3u(query)
     response = Response(stream_with_context(m3u_gen), mimetype='application/x-mpegURL')
-    response.headers['Content-Disposition'] = 'attachment; filename=gathermate.m3u'
+    response.headers['Content-Disposition'] = 'attachment; filename={}.m3u'.format(filename)
     return response
 
-@streamate.route('/epg')
+@streamate.route('/<string:filename>.xml')
 @auth.requires_auth
-def epg():
+def epg(filename):
     query = MultiDict(request.args.iteritems(multi=True))
     epg_gen = app.managers[name].order_all_epg(query)
     response = Response(stream_with_context(epg_gen), mimetype='application/xml')
-    response.headers['Content-Disposition'] = 'attachment; filename=gathermate.xml'
-    return response
-
-@streamate.route('/<string:streamer>/<string:cid>')
-@auth.requires_auth
-def streamer_channel_streaming(streamer, cid):
-    qIndex = int(request.args.get('q', -1))
-    gen = _order(streamer, 'streaming', None)
-    response = Response(stream_with_context(gen(cid, qIndex)), mimetype='video/MP2T')
-    response.headers['Content-Disposition'] = 'attachment; filename={}-{}.ts'.format(streamer, cid)
+    response.headers['Content-Disposition'] = 'attachment; filename={}.xml'.format(filename)
     return response
 
 def _order(streamer, order, query):
