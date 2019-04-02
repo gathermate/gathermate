1938fb598a3daf7117ec7bd76763a889fa22b78e
gathermate <castwhendamagedealt@gmail.com>
2019-04-02 16:54:22 +0900

    apply this site changes

--- apps/scrapmate/boards/wal.py
+++ apps/scrapmate/boards/wal.py
@@ -14,7 +14,7 @@ def register():
 
 
 class Wal(BoardScraper):
-    URL = 'https://m.torrentwal.com/'
+    URL = 'https://torrentwal.com/'
     LIST_URL = ud.join(URL, '%s/torrent1.htm')
     ID_REGEXP = re.compile(r'(\d{2,8}).html')
     PAGE_QUERY = 'torrent%d.htm'
@@ -37,67 +37,64 @@ class Wal(BoardScraper):
         # type: (fetchers.Response) -> Generator
         tree = self.etree(r, encoding=self.encoding)
 
-        list_xpath = r'//a[@class="list_subject"]'
-        for e in tree.xpath(list_xpath):
+        list_xpath = r'//table[@class="board_list"]/tr'
+        for tr in tree.xpath(list_xpath):
             try:
-                title = e.text.strip()
-                link = e.get('href')
+                a = tr.find('td[@class="subject"]/a')
+                link = a.get('href')
+                title = a.text.strip()
+                if a.getprevious() is not None:
+                    title = a.getprevious().text + ' ' + title
                 id_ = self.get_id_num(link)
-                date = e.getparent().getnext().find('a').text.strip()
+                date = ''.join(tr.find('td[@class="datetime"]').itertext())
                 yield {'id': id_, 'title': title, 'link': link, 'etc': date}
             except:
                 MyFlaskException.trace_error()
 
-        pop_xpath = r'//div[@id="m_list"]/ul/li/a'
-        root = tree.xpath(pop_xpath)
-        length = len(root)
-        for idx, e in enumerate(root):
-            try:
-                title = e.text.strip()
-                link = e.get('href')
-                id_ = length - idx
-                real_id =  self.get_id_num(link)
-                yield {'id': id_, 'title': title, 'link': link, 'etc': real_id}
-            except:
-                MyFlaskException.trace_error()
+        if ud.Url(r.url).path == '':
+            pop_xpath = r'//ol[@id="latest_popular"]/li/a'
+            root = tree.xpath(pop_xpath)
+            length = len(root)
+            for idx, e in enumerate(root):
+                try:
+                    title = e.text.strip()
+                    link = e.get('href')
+                    id_ = length - idx
+                    real_id =  self.get_id_num(link)
+                    yield {'id': id_, 'title': title, 'link': link, 'etc': real_id}
+                except:
+                    MyFlaskException.trace_error()
 
     def get_item(self, r):
         # type: (fetchers.Response) -> Generator
         root = self.etree(r, encoding=self.encoding)
 
-        item_xpath = r'//a[contains(@href, "file_download")]'
-        script_regexp = re.compile(r'javascript:file_download\([\'"](.*)[\'"]\,\s[\'"](.*)[\'"]\);')
-        for e in root.xpath(item_xpath):
+        item_xpath = r'//table[@id="file_table"]/tr/td/a'
+        script_regexp = re.compile(r'javascript:file_download\(\'(.*?)\',.?\'(.*?)\'\)')
+        location_regexp = re.compile(r'document.location.href=[\'"](.+)[\'"];')
+        for a in root.xpath(item_xpath):
             try:
-                href = e.get('href')
+                href = a.get('href')
                 match = script_regexp.search(href)
                 if match:
-                    link = ud.join(self.URL, match.group(1))
-                    name = match.group(2)
+                    second_link = ud.Url(ud.join(self.URL, match.group(1)))
+                    if second_link.path.split('/')[-1].startswith('magnet'):
+                        r = self.fetch(second_link, referer=r.url)
+                        location_match = location_regexp.search(r.content)
+                        if location_match:
+                            link = location_match.group(1)
+                            name = ud.split_qs(link)['dn']
+                            yield {'name': name, 'link': link, 'type': 'magnet'}
+                    else:
+                        link = second_link.text
+                        name = match.group(2)
+                        yield {'name': name, 'link': link, 'type': 'file'}
+                else:
+                    link = ud.join(self.URL, href)
+                    name = re.sub(r'\(.+\)$', '', a.xpath('string()').strip()).strip()
                     yield {'name': name, 'link': link, 'type': 'file'}
-
-                a = e.getparent().getnext().find('a')
-                if a is not None:
-                    link = a.get('href')
-                    if tb.is_magnet(link):
-                        yield {'name': name, 'link': link, 'type': 'magnet'}
-
             except:
                 MyFlaskException.trace_error()
-        '''
-        magnet_xpath = r'//input[contains(@value, "magnet:?xt")]'
-        for e in root.xpath(magnet_xpath):
-            try:
-                value = e.get('value')
-                query = ud.Url(value)
-                xt = query.query_dict.get('xt', [])
-                name = query.query_dict.get('dn', [])
-                if xt and len(xt) > 10:
-                    name = name if name else xt
-                    yield {'name': name, 'link': ud.unquote(value), 'type': 'magnet'}
-            except:
-                MyFlaskException.trace_error()
-        '''
 
     def get_file(self, url, ticket):
         # type: (urldealer.Url, Type[Dict[Text, Union[Text, List[Text]]]]) -> fetchers.Response
